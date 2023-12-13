ee77def1f473f9629cd59cfd8dd4bc35504de8ed
Black854-notebook <juststepup854@gmail.com>
2023-12-13 23:11:55 +0600

    refactoring. переделал задачник оборудования

--- src/components/WorkList/taskComponents/EquipTasks.tsx
+++ src/components/WorkList/taskComponents/EquipTasks.tsx
@@ -1,5 +1,5 @@
 import { DeleteOutlined, UploadOutlined, FileWordOutlined, PrinterOutlined } from '@ant-design/icons'
-import { Typography, Table, TableColumnsType, Button, Popconfirm, Col, Row, Modal } from "antd"
+import { Typography, Table, TableColumnsType, Button, Popconfirm, Col, Row, Modal, Input, Space } from "antd"
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../../redux/store'
 import { ExpandedDataType } from '../types'
@@ -9,7 +9,6 @@ import { ProtocolUpload } from './MiniComponents/ProtocolUpload'
 import { ProtocolCode } from './MiniComponents/ProtocolCode'
 import { ReportCode } from './MiniComponents/ReportCode'
 import { LabelStatus } from './MiniComponents/LabelStatus'
-import { ProgressHelper } from './ProgressHelper'
 import { PamUpload } from './MiniComponents/PamUpload'
 import { PamStatus } from './MiniComponents/PamStatus'
 import { ReportUpload } from './MiniComponents/ReportUpload'
@@ -397,12 +396,23 @@ export const EquipTasks: React.FC<EquipTasks> = ({ myEquipDataIdArray, myEquipDa
             dataIndex: 'rowName',
             render: (rowName: string) => <Text style={{ fontSize: '12pt' }} >{rowName}</Text>,
         },
+        {
+            dataIndex: 'value',
+            width: '60%'
+        },
     ]
 
     const [iframeKey, setIframeKey] = useState(0)
     const [BnModalOpen, setBnModalOpen] = useState(false)
     const [CDConvertModalOpen, setCDConvertModalOpen] = useState(false)
     const [TeachProtocolModalOpen, setTeachProtocolModalOpen] = useState(false)
+    const [TitleListModalOpen, setTitleListModalOpen] = useState(false)
+    const [TitlesCDListModalOpen, setTitlesCDListModalOpen] = useState(false)
+
+    const [AddChar, setAddChar] = useState('')
+    const [AddName, setAddName] = useState('')
+
+    const [AddsChars, setAddsChars] = useState('')
 
     const handleCancel = (modalType: string) => {
         if (modalType === 'BN') {
@@ -414,51 +424,70 @@ export const EquipTasks: React.FC<EquipTasks> = ({ myEquipDataIdArray, myEquipDa
         } else if (modalType === 'TeachProtocol') {
             setTeachProtocolModalOpen(false)
             setIframeKey(prevKey => prevKey + 1)
+        } else if (modalType === 'TitleList') {
+            setTitleListModalOpen(false)
+            setIframeKey(prevKey => prevKey + 1)
+        } else if (modalType === 'TitlesCDList') {
+            setTitlesCDListModalOpen(false)
+            setIframeKey(prevKey => prevKey + 1)
         }
     }
 
     const AddsData = [
         {
-            rowName: <>
-                Бланк несоответствия
-                <Button onClick={() => { setBnModalOpen(true) }} style={{ marginLeft: '10px' }} type='link' icon={<PrinterOutlined />} />
+            rowName: <Text style={{ fontSize: '12pt' }}>Бланк несоответствия</Text>,
+            value: <>
+                <Button onClick={() => { setBnModalOpen(true) }} style={{ borderRadius: '0', width: '100%' }} type='primary' icon={<PrinterOutlined />} >Печать</Button>
                 <Modal title="Бланк несоответствия" open={BnModalOpen} onCancel={() => handleCancel('BN')} footer={[<Button key="close" onClick={() => handleCancel('BN')} type="primary">Закрыть</Button>]} >
                     <iframe key={iframeKey} style={{ width: '90%', height: '70vh', marginLeft: '5%' }} src={`http://10.85.10.212/ov/API/PrintForms/bn.pdf`}></iframe>
                 </Modal>
             </>
         },
         {
-            rowName: <>
-                Протокол обучения персонала
-                <Button onClick={() => { setTeachProtocolModalOpen(true) }} disabled={data[0].nvp === '' || data[0].dvp === ''} style={{ marginLeft: '10px' }} type='link' icon={<PrinterOutlined />} />
+            rowName: <Text style={{ fontSize: '12pt' }}>Протокол обучения персонала</Text>,
+            value: <>
+                <Button onClick={() => { setTeachProtocolModalOpen(true) }} disabled={data[0].nvp === '' || data[0].dvp === ''} style={{ borderRadius: '0', width: '100%' }} type='primary' icon={<PrinterOutlined />} >Печать</Button>
                 <Modal afterOpenChange={() => handleCancel('TeachProtocol')} title="Протокол обучения персонала" open={TeachProtocolModalOpen} onCancel={() => handleCancel('TeachProtocol')} footer={[<Button key="close" onClick={() => handleCancel('TeachProtocol')} type="primary">Закрыть</Button>]} >
                     <iframe
-                    key={iframeKey}
-                    style={{ width: '100%', height: '40vh', marginLeft: '5%' }}
-                    src={`http://10.85.10.212/ov/API/PrintForms/add_b.php?id=${data[0].id}&idfromtable=${data[0].idfromtable}&tp=equip.work&user&stroki=5&typeForm=2`}></iframe>
+                        key={iframeKey}
+                        style={{ width: '100%', height: '40vh', marginLeft: '5%' }}
+                        src={`http://10.85.10.212/ov/API/PrintForms/add_b.php?id=${data[0].id}&idfromtable=${data[0].idfromtable}&tp=equip.work&user&stroki=5&typeForm=2`}></iframe>
                 </Modal>
             </>
         },
         {
-            rowName: <>
-                Титульные листы приложений
-                <Button disabled={data[0].nvp === ''} style={{ marginLeft: '10px' }} type='link' icon={<PrinterOutlined />} />
+            rowName: <Text style={{ fontSize: '12pt' }}>Титульные листы приложений</Text>,
+            value: <>
+                <Space.Compact style={{ width: '100%' }}>
+                    <Input placeholder="Буква" value={AddChar} onChange={(e) => { setAddChar(e.currentTarget.value) }} style={{ width: '140px' }} allowClear />
+                    <Input placeholder="Наименование" value={AddName} onChange={(e) => { setAddName(e.currentTarget.value) }} allowClear />
+                    <Button onClick={() => { setTitleListModalOpen(true) }} disabled={data[0].nvp === '' || AddName === '' || AddChar === ''} type='primary' icon={<PrinterOutlined />} >Печать</Button>
+                    <Modal afterOpenChange={() => handleCancel('TitleList')} title="Титульные листы приложений" open={TitleListModalOpen} onCancel={() => handleCancel('TitleList')} footer={[<Button key="close" onClick={() => handleCancel('TitleList')} type="primary">Закрыть</Button>]} >
+                        <iframe key={iframeKey} style={{ width: '90%', height: '70vh', marginLeft: '5%' }} src={`http://10.85.10.212/ov/API/PrintForms/add_any.php?nvp=${data[0].nvp}&name=${AddName}&char=${AddChar}`}></iframe>
+                    </Modal>
+                </Space.Compact>
             </>
         },
         {
-            rowName: <>
-                Титульный лист для приложений на диске
-                <Button disabled={data[0].nvp === ''} style={{ marginLeft: '10px' }} type='link' icon={<PrinterOutlined />} />
-            </>
-        },
-        {
-            rowName: <>
-                Конверт для диска
-                <Button onClick={() => { setCDConvertModalOpen(true) }} style={{ marginLeft: '10px' }} type='link' icon={<PrinterOutlined />} />
-                <Modal title="Конверт для диска" open={CDConvertModalOpen} onCancel={() => handleCancel('CDConvert')} footer={[<Button key="close" onClick={() => handleCancel('CDConvert')} type="primary">Закрыть</Button>]} >
-                    <iframe key={iframeKey} style={{ width: '90%', height: '70vh', marginLeft: '5%' }} src={`http://10.85.10.212/ov/API/PrintForms/CD.pdf`}></iframe>
+            rowName: <Text style={{ fontSize: '12pt' }}>Титульный лист (для диска)</Text>,
+            value: <>
+            <Space.Compact style={{ width: '100%' }}>
+                <Input placeholder="Буквы через запятую" value={AddsChars} onChange={(e) => { setAddsChars(e.currentTarget.value) }} allowClear />
+                <Button onClick={() => { setTitlesCDListModalOpen(true) }} disabled={data[0].nvp === '' || AddsChars === ''} type='primary' icon={<PrinterOutlined />} >Печать</Button>
+                <Modal afterOpenChange={() => handleCancel('TitlesCDList')} title="Титульные листы приложений на диске" open={TitlesCDListModalOpen} onCancel={() => handleCancel('TitlesCDList')} footer={[<Button key="close" onClick={() => handleCancel('TitlesCDList')} type="primary">Закрыть</Button>]} >
+                    <iframe key={iframeKey} style={{ width: '90%', height: '70vh', marginLeft: '5%' }} src={`http://10.85.10.212/ov/API/PrintForms/add_cde.php?nvp=${data[0].nvp}&chars=${AddsChars}`}></iframe>
                 </Modal>
-            </>
+            </Space.Compact>
+        </>
+        },
+        {
+            rowName: <Text style={{ fontSize: '12pt' }}>Конверт для диска</Text>,
+            value: <>
+            <Button onClick={() => { setCDConvertModalOpen(true) }} style={{ borderRadius: '0', width: '100%' }} type='primary' icon={<PrinterOutlined />} >Печать</Button>
+            <Modal title="Конверт для диска" open={CDConvertModalOpen} onCancel={() => handleCancel('CDConvert')} footer={[<Button key="close" onClick={() => handleCancel('CDConvert')} type="primary">Закрыть</Button>]} >
+                <iframe key={iframeKey} style={{ width: '90%', height: '70vh', marginLeft: '5%' }} src={`http://10.85.10.212/ov/API/PrintForms/CD.pdf`}></iframe>
+            </Modal>
+        </>,
         },
     ]
 
@@ -467,7 +496,7 @@ export const EquipTasks: React.FC<EquipTasks> = ({ myEquipDataIdArray, myEquipDa
             <Col span={3} push={1} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }}>
                 <ProgressStatus record={rec} myEquipData={myEquipData} />
             </Col>
-            <Col span={12} push={2}>
+            <Col span={9} push={2}>
                 <Table
                     columns={columns}
                     dataSource={
@@ -487,7 +516,7 @@ export const EquipTasks: React.FC<EquipTasks> = ({ myEquipDataIdArray, myEquipDa
                     size="small"
                 />
             </Col>
-            <Col span={6} push={2}>
+            <Col span={9} push={2}>
                 <Table
                     columns={AddsColumns}
                     dataSource={AddsData}
